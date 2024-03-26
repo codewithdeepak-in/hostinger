b0d313dbe20f28e34960c239aa037e83035e5ce7
Deepak Chaudhary <101586061+codewithdeepak-in@users.noreply.github.com>
2024-03-26 20:30:18 +0530

    message

---
+++ assets/js/index.js
@@ -0,0 +1,33 @@
+function togglePanel() {
+    var x = document.querySelector(".navigation-panel");
+    var y = document.querySelector('.content-panel')
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        x.style.width = "100%";
+        y.style.display = "none";
+    } else {
+        x.style.display = "none";
+        y.style.display = "block";
+        y.style.width = "100%"
+    }
+}
+var navButtons = document.querySelectorAll('.nav-content');
+navButtons.forEach(function (button) {
+    button.addEventListener('click', function () {
+        var value = this.getAttribute('data-value');
+        fetchContent(value);
+        togglePanel();
+    });
+});
+
+function fetchContent(value) {
+    // Assuming the HTML files are located in a folder named "html"
+    fetch('sections/' + value + '.html') // Fetch the HTML file corresponding to the value
+        .then(response => response.text())
+        .then(data => {
+            document.querySelector('.content-panel').innerHTML = data; // Insert the fetched HTML content into the content panel
+        })
+        .catch(error => console.error('Error fetching content:', error));
+}
+
+navButtons[0].click();
